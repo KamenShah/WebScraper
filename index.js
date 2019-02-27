ad03fa5caeb9a8386433b7032dbde16c01f5ef60
Kamen Shah <kamenshah@gmail.com>
2019-02-27 16:36:50 -0700

    Comment file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Created by Kamen SHah
+
 var cheerio = require('cheerio');
 var axios = require('axios');
 var util = require('util')
@@ -6,6 +8,7 @@ var WEXurl = "https://www.sourcewell-mn.gov/cooperative-purchasing/022217-wex";
 getHTML(WEXurl);
 
 
+// Use axios Library to retrieve html from webpage
 function getHTML(url){
     axios.get(url).then(response => {
         parseHTML(response.data);
@@ -14,6 +17,7 @@ function getHTML(url){
     });
 }
 
+//Function that generates JSON object using Cheerio Library to parse HTML
 function parseHTML(html){
         var links = [];
         var contacts = [];
@@ -27,6 +31,7 @@ function parseHTML(html){
         expirationDate = expirationDate.trim();
         expirationDate = expirationDate.split(" ")[2];
 
+        // Get links to all documents on webpage
         $('.file-link').each(function(i, element){
             var link = $(this).children().attr('href');
             var linkName = $(this).children().text();
@@ -34,6 +39,7 @@ function parseHTML(html){
 
         });
 
+        // Get user data for all users 
         $('.inline-user').each(function(i, element){
             var user = $(this).text().replace(/^\s*\n/gm, '').split('\n');
             var name = user[0].trim();
@@ -44,6 +50,7 @@ function parseHTML(html){
               }
         });
         
+        // Generate JSON Object
         var jsonObject = {
             title: title,
             expiration: expirationDate,
@@ -55,6 +62,46 @@ function parseHTML(html){
           };
         console.log(util.inspect(jsonObject, {showHidden: false, depth: null}))
 
-        //console.log(jsonObject);
-
 }
+
+/*
+JSON object created from script
+
+{ title: 'Fleet GPS & Telematics',
+  expiration: '06/01/2021',
+  contract_number: '#022217-WEX',
+  files: 
+   [ { linkName: 'Request for Proposal (RFP)',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Fleet%20Management%20%20RFP%20022217_5.pdf' },
+     { linkName: 'Contract Forms ',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/WEX%20Contract%20022217.pdf' },
+     { linkName: 'Contract Acceptance & Award',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Acceptance%20and%20Award-WEX%20022217.pdf' },
+     { linkName: 'Affidavit of Advertisement',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Combined%20Ads-Fleet%20Management%20022217_6.pdf' },
+     { linkName: 'Proposal Opening Witness Page',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Proposal%20Opening%20Witness-Fleet%20Management%20022217_5.pdf' },
+     { linkName: 'Proposal Evaluation',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Form%20G%20Evaluation-Fleet%20Management%20022217_5.pdf' },
+     { linkName: 'Evaluation Committee Comment & Review',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Comment%20and%20Review-Fleet%20Management%20022217_5.pdf' },
+     { linkName: 'Board Minutes',
+       link: 'https://www.sourcewell-mn.gov/sites/default/files/2018-05/Board%20Minutes%206-20-17_7.pdf' } ],
+  vendor: 
+   { name: 'WEX',
+     contacts: 
+      [ { name: 'Denise Baumgart',
+          phone: '888-842-0075',
+          email: ' denise.baumgart@wexinc.com' },
+        { name: 'Kelly McAllister, Contract Administration Supervisor',
+          phone: '218-894-5468',
+          email: 'kelly.mcallister@sourcewell-mn.gov' },
+        { name: 'Kim Wudinich, Contract Administration Specialist',
+          phone: '218-894-5485',
+          email: 'kim.wudinich@sourcewell-mn.gov' } 
+      ]
+    } 
+}
+
+
+*/
